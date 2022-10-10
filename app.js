f0b9005a6a72b0c6153a87e41d24fc9edcc7ab84
StephanieRuthJones <stephanieruthjones@gmail.com>
2022-10-10 10:14:34 -0600

    Add question note to app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ cartTotalDisplay.innerText = cartTotal;
 //Use a for...of loop to iterate over the foodMenuItems array and create menu item cards for each of them.
 
 //Use the for...in loop to iterate over the drinkMenuItems array and create menu item cards for each of them.
+//Question: Why is it less ideal to use a for...in loop (as opposed to a for...of loop) to iterate over an array?
 
 //Use a .map() method to iterate over the dessertMenuItems array and create menu item cards for each of them.
 
