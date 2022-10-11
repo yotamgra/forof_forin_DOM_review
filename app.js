a06fde6894da2ecd180cacb81f08f90cc2e18741
Yotam Granada <yotam.granada@gmail.com>
2022-10-11 15:40:04 +0300

    exercise completed

--- app.js
+++ app.js
@@ -6,6 +6,135 @@ const cartTotalDisplay = document.querySelector(".cart-total");
 cartTotalDisplay.innerText = cartTotal;
 
 //Use a for...of loop to iterate over the foodMenuItems array and create menu item cards for each of them.
+function createFoodMenu() {
+  for (const item of foodMenuItems) {
+    const { name, image, description, price } = item;
+    foodMenuContainer.innerHTML += `<div class="card">
+        <div class="card-header">
+            <h2>${name}</h2>
+        </div>
+        <div class="card-body">
+            <div class="card-body-left">
+                <img
+                    src="${image}"
+                    alt="${name}"
+                />
+            </div>
+            <div class="card-body-right">
+                <p>
+                ${description}
+                </p>
+            </div>
+            </div>
+            <div class="card-footer">
+                <p class="price">$10</p>
+                <button value=${price}>Add to Cart</button>
+            </div>
+        </div>
+    </div>`;
+  }
+}
+// createFoodMenu();
+
+function createDrinkMenu() {
+  for (const drinkIndex in drinkMenuItems) {
+    const { name, image, description, price } = drinkMenuItems[drinkIndex];
+    drinkMenuContainer.innerHTML += `<div class="card">
+        <div class="card-header">
+            <h2>${name}</h2>
+        </div>
+        <div class="card-body">
+            <div class="card-body-left">
+                <img
+                    src="${image}"
+                    alt="${name}"
+                />
+            </div>
+            <div class="card-body-right">
+                <p>
+                ${description}
+                </p>
+            </div>
+            </div>
+            <div class="card-footer">
+                <p class="price">$10</p>
+                <button value=${price}>Add to Cart</button>
+            </div>
+        </div>
+    </div>`;
+  }
+}
+// createDrinkMenu();
+
+function createDessertMenu() {
+  dessertMenuItems.map((item) => {
+    const { name, image, description, price } = item;
+    dessertMenuContainer.innerHTML += `<div class="card">
+        <div class="card-header">
+            <h2>${name}</h2>
+        </div>
+        <div class="card-body">
+            <div class="card-body-left">
+                <img
+                    src="${image}"
+                    alt="${name}"
+                />
+            </div>
+            <div class="card-body-right">
+                <p>
+                ${description}
+                </p>
+            </div>
+            </div>
+            <div class="card-footer">
+                <p class="price">$10</p>
+                <button value=${price}>Add to Cart</button>
+            </div>
+        </div>
+    </div>`;
+  });
+}
+// createDessertMenu();
+
+function createMenu(container, menuItems) {
+  menuItems.forEach((item) => {
+    const { name, price, description, image } = item;
+    container.innerHTML += `<div class="card">
+            <div class="card-header">
+                <h2>${name}</h2>
+            </div>
+            <div class="card-body">
+                <div class="card-body-left">
+                    <img
+                        src="${image}"
+                        alt="${name}"
+                    />
+                </div>
+                <div class="card-body-right">
+                    <p>
+                    ${description}
+                    </p>
+                </div>
+                </div>
+                <div class="card-footer">
+                    <p class="price">$${price}</p>
+                    <button class="add-to-cart" value=${price}>Add to Cart</button>
+                </div>
+            </div>
+        </div>`;
+  });
+}
+createMenu(foodMenuContainer, foodMenuItems);
+createMenu(drinkMenuContainer, drinkMenuItems);
+createMenu(dessertMenuContainer, dessertMenuItems);
+
+const buttons = document.querySelectorAll(".add-to-cart");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    cartTotal += +button.value;
+    cartTotalDisplay.innerText = cartTotal;
+  });
+});
 
 //Use the for...in loop to iterate over the drinkMenuItems array and create menu item cards for each of them.
 //Question: Why is it less ideal to use a for...in loop (as opposed to a for...of loop) to iterate over an array?
--- data.js
+++ data.js
@@ -24,50 +24,31 @@ const foodMenuItems = [
       "https://images.unsplash.com/photo-1598866594230-a7c12756260f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1608&q=80",
   },
 ];
-const drinkMenuItems = {
-  Coke: {
+
+
+const drinkMenuItems = [
+  {
+    name: "Coke",
     price: 2.0,
     description: "A delicious coke with ice.",
     image:
       "https://images.unsplash.com/photo-1581996323441-538096e854b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y3VwJTIwb2YlMjBzb2RhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
   },
-  Sprite: {
+  {
+    name: "Sprite",
     price: 2.0,
     description: "A delicious sprite with ice.",
     image:
       "https://images.unsplash.com/photo-1581996323441-538096e854b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y3VwJTIwb2YlMjBzb2RhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
   },
-  Fanta: {
+  {
+    name: "Fanta",
     price: 2.0,
     description: "A delicious fanta with ice.",
     image:
       "https://images.unsplash.com/photo-1581996323441-538096e854b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y3VwJTIwb2YlMjBzb2RhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
   },
-};
-
-// const drinkMenuItems = [
-//   {
-//     name: "Coke",
-//     price: 2.0,
-//     description: "A delicious coke with ice.",
-//     image:
-//       "https://images.unsplash.com/photo-1581996323441-538096e854b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y3VwJTIwb2YlMjBzb2RhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-//   },
-//   {
-//     name: "Sprite",
-//     price: 2.0,
-//     description: "A delicious sprite with ice.",
-//     image:
-//       "https://images.unsplash.com/photo-1581996323441-538096e854b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y3VwJTIwb2YlMjBzb2RhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-//   },
-//   {
-//     name: "Fanta",
-//     price: 2.0,
-//     description: "A delicious fanta with ice.",
-//     image:
-//       "https://images.unsplash.com/photo-1581996323441-538096e854b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y3VwJTIwb2YlMjBzb2RhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-//   },
-// ];
+];
 
 const dessertMenuItems = [
   {
